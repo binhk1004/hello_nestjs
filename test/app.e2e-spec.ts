e66ef3dbdac99db2f98777460ff3ff8cf19c5120
binhk1004 <binhk1004@naver.com>
2023-02-17 22:33:37 +0900

    e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,21 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcom to My Movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'Test', year: 2000, genres: ['test'] })
+        .expect(201);
+    });
+    it('삭제', () => {
+      return request(app.getHttpServer()).delete('/movie').expect(404);
+    });
   });
 });
